f53e5767ad6090c7ec41bbc1b2619dd404e7b075
Geo Roa <geojayroa@yahoo.com>
2015-02-03 00:33:26 -0600

    Updated Problem 5 and 6 to make it more user friendly.

--- Problem_5.py
+++ Problem_5.py
@@ -1,14 +1,26 @@
-p1 = Policy('Policy Four', date(2015, 2, 1), 500)
-p1.billing_schedule = 'Two-Pay'
 
-john = Contact.query.filter_by(name='John Doe')\
+policy = raw_input('Enter the policy name you want to add: ')
+year = raw_input('Enter the effective date year: ')
+month  = raw_input('Enter the effective date month (Must include leading zeros if any!): ')
+day = raw_input('Enter the effective date day (Must include leading zeros if any!): ')
+premium = raw_input('Enter the annual premium: ')
+billing = raw_input('Enter the billing schedule : ')
+agent_input = raw_input('Enter the agents name (Contact must exist!): ')
+insured_input = raw_input('Enter the insured name (Contact must exist!): ')
+
+p1 = Policy(policy, date(int(year), int(month), int(day)), int(premium))
+p1.billing_schedule = billing
+
+q1 = Contact.query.filter_by(name=agent_input)\
 .filter(Contact.role == 'Agent').first()
-p1.agent = john.id
+p1.agent = q1.id
 
-ryan = Contact.query.filter_by(name ='Ryan Bucket')\
+q2 = Contact.query.filter_by(name =insured_input)\
 .filter(Contact.role == 'Named Insured').first()
-p1.named_insured = ryan.id
+p1.named_insured = q2.id
 
 db.session.add(p1)
 db.session.commit()
+
+print("Policy added!")
 
--- Problem_6.py
+++ Problem_6.py
@@ -1,6 +1,11 @@
-john = Contact.query.filter_by(name='John Doe')\
+policy = raw_input('Enter the policy number you want to update (Policy must exist!): ')
+add_name = raw_input('Enter the name of the insured person you want to add (Contact must exist!): ')
+
+q1 = Contact.query.filter_by(name=add_name)\
 .filter(Contact.role == 'Named Insured').first()
 
-db.session.query(Policy).filter_by(policy_number ='Policy One').update({"named_insured": john.id})
+db.session.query(Policy).filter_by(policy_number =policy).update({"named_insured": q1.id})
 
 db.session.commit()
+
+print("Insured name added!")
